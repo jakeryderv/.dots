8b488264c9d9a491d0e622fef98dc9e37569de5a
Jake Van Slyke <jakervanslyke@gmail.com>
2025-12-16 08:14:25 -0600

    update

--- nvim/.config/nvim/lua/custom/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/custom/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
   version = '*',
   config = function()
     require('toggleterm').setup({
-      open_mapping = [[<c-\>]],
+      open_mapping = [[<leader>t]],
       direction = 'float', -- float | horizontal | vertical | tab
       shade_terminals = true,
       start_in_insert = true,
