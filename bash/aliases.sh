bf11083abd513a9ca6757cbdecca4087de2842d9
Jake Van Slyke <jakervanslyke@gmail.com>
2025-12-16 23:05:33 -0600

    add bs aliases

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -21,9 +21,18 @@ alias tma='tmux attach -t $(tmux ls -F "#{session_name}" | fzf)'
 alias bat='batcat'
 
 # Eza
-alias l='eza --icons --group-directories-first'
-alias ls='eza --icons --group-directories-first'
-alias ll='eza -l --icons --group-directories-first --git'
-alias la='eza -la --icons --group-directories-first --git'
-alias lt='eza --tree --level=2 --icons'
-alias lg='eza -l --icons --git'
+# alias l='eza --icons --group-directories-first'
+# alias ls='eza --icons --group-directories-first'
+# alias ll='eza -l --icons --group-directories-first --git'
+# alias la='eza -la --icons --group-directories-first --git'
+# alias lt='eza --tree --level=2 --icons'
+# alias lg='eza -l --icons --git'
+
+# bs - custom ls tooling
+alias l='bs -nLT --icons --relative'
+alias la='bs -anLT --icons --relative'
+alias lw='bs -nLT --icons --relative --watch'
+alias ltree='bs -R --tree --depth'
+
+# fallbask basic ls stuff
+alias ls='ls --color=auto'
