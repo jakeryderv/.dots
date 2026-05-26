cb368a97f3c61ad235bdc74f6cf407d0473d810a
Jake Van Slyke <jakervanslyke@gmail.com>
2026-05-26 18:41:55 -0500

    wezterm - set tab styling stuff

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,12 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font = wezterm.font '0xProto Nerd Font'
-config.color_scheme = 'carbonfox'
+config.font = wezterm.font("0xProto Nerd Font")
+config.color_scheme = "carbonfox"
+config.window_decorations = "RESIZE"
+
+config.use_fancy_tab_bar = true
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
