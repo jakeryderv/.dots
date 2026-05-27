1239ab4681b3f1e65fbaadcf6655f253ae81e556
Jake Van Slyke <jakervanslyke@gmail.com>
2026-05-26 20:14:26 -0500

    wezterm - 0xProto Nerd Font Mono

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("0xProto Nerd Font")
+config.font = wezterm.font("0xProto Nerd Font Mono")
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.font_size = 12.0
 config.font_dirs = {}
