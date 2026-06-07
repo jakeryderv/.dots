763f07c2d25f2c5b74c4ce2430b998b2bbda2df9
Jake Van Slyke <jakervanslyke@gmail.com>
2026-06-07 16:03:57 -0500

    nvim - change blink.cmp keymap preset to super-tab

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -19,7 +19,7 @@ return {
   --- @module 'blink.cmp'
   --- @type blink.cmp.Config
   opts = {
-    keymap = { preset = 'default' },
+    keymap = { preset = 'super-tab' },
     appearance = { nerd_font_variant = 'mono' },
     completion = {
       documentation = { auto_show = false, auto_show_delay_ms = 500 },
