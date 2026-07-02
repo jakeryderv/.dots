e4d9e14c26b7b00eee707e10630735fe4a2c2677
Jake Van Slyke <jakervanslyke@gmail.com>
2026-07-02 12:38:14 -0500

    style(exports): shfmt normalization after PATH-guard edit

--- _bash/exports.sh
+++ _bash/exports.sh
@@ -3,18 +3,18 @@
 
 # Path (guard against duplication when sourced from non-login subshells)
 case ":$PATH:" in
-    *":$HOME/.local/bin:"*) ;;
-    *) export PATH="$HOME/.local/bin:$PATH" ;;
+*":$HOME/.local/bin:"*) ;;
+*) export PATH="$HOME/.local/bin:$PATH" ;;
 esac
 
 # ls colors (LS_COLORS)
 if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+	test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
 fi
 
 # Editor
 if [[ -n $SSH_CONNECTION ]]; then
-    export EDITOR='vim'
+	export EDITOR='vim'
 else
-    export EDITOR='nvim'
+	export EDITOR='nvim'
 fi
