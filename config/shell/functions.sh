33a74b07833f6a9f35c7fe0f2f702db4af9f9ca2
Jake Van Slyke <jakervanslyke@gmail.com>
2026-09-18 18:19:36 -0500

    shell: daily-dev helper function

--- config/shell/functions.sh
+++ config/shell/functions.sh
@@ -26,8 +26,31 @@ up() {
 
 daily() {
     local dir
-    dir="$HOME/daily-dev/log/$(date +%F)"
+    local readme
+    local today
+
+    today="$(date +%F)"
+    dir="$HOME/daily-dev/log/$today"
+    readme="$dir/README.md"
 
     mkdir -p "$dir"
+
+    if [[ ! -f "$readme" ]]; then
+        cat >"$readme" <<EOF
+---
+date: $today
+projects: []
+tags: []
+---
+
+# $today
+
+## Worked On
+
+## Notes
+EOF
+    fi
+
     cd "$dir" || return
+    "${EDITOR:-nvim}" README.md
 }
